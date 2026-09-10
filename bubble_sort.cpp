cfcb5cff7599a7a9221fb8b096bbf04d3f5374e0
Nishat Ahsan <ahsannishat01@gmail.com>
2026-09-10 19:47:58 +0600

    fixed file name to bubble_sort.cpp

---
+++ bubble_sort.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+void bubbleSort(int arr[],int n){
+
+    for(int i=0; i<n-1; i++){
+        bool isSwap = false;
+        for(int j=0; j< n-i-1; j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j], arr[j+1]);
+                isSwap = true;
+            }
+
+        }
+        if(!isSwap){
+            return;
+        }
+    }
+}
+
+void printArray(int arr[],int n){
+    for(int i=0; i<n; i++){
+        cout << arr[i] << " " ;
+    }
+    cout << endl;
+}
+
+int main(){
+
+    int arr[]={1,2,5,4,3,6,10,19};
+    int n= sizeof(arr)/sizeof(arr[0]);
+    bubbleSort(arr, n);
+    printArray(arr,n);
+
+    return 0;
+
+}
