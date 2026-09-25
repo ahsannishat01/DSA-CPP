992c8332e3004a4d709328e65baf9544869f0878
Nishat Ahsan <ahsannishat01@gmail.com>
2026-09-25 22:47:29 +0600

    codeforce_1180A

---
+++ codeforce_1180A.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+        int answer = {n * n + (n - 1) * (n - 1)};
+        cout << answer << endl;
+    
+    return 0;
+}
